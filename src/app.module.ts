2f4efc23257bce7ed252fdae534dc2cdb99b5f77
salabibne <salabyaham@gmail.com>
2025-05-04 09:59:31 +0600

    feat review feature added

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { UsersModule } from './users/users.module';
 import { SportsServiceModule } from './sports-service/sports-service.module';
 import { PaymentModule } from './payment/payment.module';
 import { AnnouncementModule } from './announcement/announcement.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
   imports: [
@@ -15,14 +16,17 @@ import { AnnouncementModule } from './announcement/announcement.module';
     FormModule,
     MongooseModule.forRoot(
       
-      // 'mongodb+srv://sicoarena:16MhXpiIOW5LMoKr@cluster0.ovwhpk1.mongodb.net/sicoarena',
-      'mongodb+srv://sicoarena:hwHQxxwMRF5y1Hin@cluster0.8smdi.mongodb.net/sicoarena',
+      'mongodb+srv://sicoarena:16MhXpiIOW5LMoKr@cluster0.ovwhpk1.mongodb.net/sicoarena',
+      // TODO 
+      // replace clients uri
+      // 'mongodb+srv://sicoarena:hwHQxxwMRF5y1Hin@cluster0.8smdi.mongodb.net/sicoarena',
 
     ),
     UsersModule,
     SportsServiceModule,
     PaymentModule,
     AnnouncementModule,
+    ReviewsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
