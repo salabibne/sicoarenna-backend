3fa202aa45684ed981c065a426d47f91d32c20fe
salabibne <salabyaham@gmail.com>
2025-08-21 12:40:10 +0600

    added cors policy for localhost too

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://sicoarena.onrender.com',
+   origin: [
+    'https://sicoarena.onrender.com', // deployed frontend
+    'http://localhost:5173',          // local dev (Vite/React)
+  ],
   });
   await app.listen(3000);
 }
